87cc6cc8406f3560652d807645ec5e6bf267f63e
Luis <luiscarlos1919y@gmail.com>
2022-07-02 00:15:45 -0600

    Primer commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,34 @@
+import {useState} from 'react';
+import { AddCategory, GifGrid } from './components';
+
+export const GifExpertApp = () => {
+  
+    const [categories, setCategories] = useState(['One Punch']);
+
+    const onAddCategory = ( onNewCategory ) => {
+        
+        if( categories.includes(onNewCategory) ){
+            return;
+        }
+        setCategories([ onNewCategory, ...categories ]);
+    }
+
+    return (
+    <>
+        <h1>GifExpertApp</h1>
+
+        <AddCategory 
+            onNewCategory={ (value) => onAddCategory(value)}
+        />
+
+        { 
+            categories.map( (category) => (
+                <GifGrid 
+                    key={ category } 
+                    category={ category } 
+                />
+            ))
+        }
+    </>
+  )
+}
